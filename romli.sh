ecfecc379cb56a8eb02416698f712f36c672587d
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-07-20 10:16:57 +0700

    Create romli.sh

---
+++ romli.sh
@@ -0,0 +1,53 @@
+clear
+# Variableb
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+echo "##################################"|lolcat
+echo "/ TOOLS : SPAM v1 byRomli        /"|lolcat
+echo "/ ingyahh : Zxyyu Romm           /"|lolcat
+echo "/ Sokin :  Cipatik,Bandung Barat /"|lolcat
+echo "/ Recode : ROMLI NGABBBBB        /"|lolcat
+echo "##################################"|lolcat
+
+echo "daftar tools spam"
+echo "[1] Bom Chat Whatsapp"
+echo "[2] Spam Telpon"
+echo "[3] Keluar"
+read -p "PILIH SALAH SATU NGAB!! : " bro
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+php mdr2.php
+sh romli.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 01 ]
+then
+clear
+php mdr1.php
+sh romli.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+echo "\033[31;1mGwehj Romli Banh, aowkwkwk"
+sleep 1
+echo "\033[32;1mTerimakasih Sudah Menggunakan Tools ini"
+sleep 1
+echo "\033[35;1mSpam By Romli Ngab!!"
+sleep 1
+exit
+fi
